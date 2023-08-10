b9145cdf2a048511648bb220a1bf5dcaea7b82f2
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-10 23:27:46 +0100

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
-""" 
-    getting the length of passed arguments 
-    in command line 
-"""
-num_args = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-if num_args - 1 != 3:
-    print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
-    exit(1)
+    """ 
+        getting the length of passed arguments 
+        in command line 
+    """
+    num_args = len(sys.argv)
 
-else:
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if operator == '+':
-        result = add(a, b)
-        print(f"{a} {operator} {b} = {result}")
-
-    elif operator == '-':
-        result = sub(a, b)
-        print(f"{a} {operator} {b} = {result}")
-
-    elif operator == '*':
-        result = mul(a, b)
-        print(f"{a} {operator} {b} = {result}")
-
-    elif operator == '/':
-        result = div(a, b)
-        print(f"{a} {operator} {b} = {result}")
+    if num_args - 1 != 3:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        exit(1)
 
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if operator == '+':
+            result = add(a, b)
+            print(f"{a} {operator} {b} = {result}")
+
+        elif operator == '-':
+            result = sub(a, b)
+            print(f"{a} {operator} {b} = {result}")
+
+        elif operator == '*':
+            result = mul(a, b)
+            print(f"{a} {operator} {b} = {result}")
+
+        elif operator == '/':
+            result = div(a, b)
+            print(f"{a} {operator} {b} = {result}")
+
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
