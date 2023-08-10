af1e308ace1a1fd32c6643e1deb51bc55227dbca
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-10 22:44:12 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-i = 1
-result = 0
-num_args = len(sys.argv)
+    i = 1
+    result = 0
+    num_args = len(sys.argv)
 
-if num_args == 1:
-    print("{}".format(result))
-else:
-    while i > 0 and i < num_args:
-        result = result + int(sys.argv[i])
-        i = i + 1
-    print("{}".format(result))
+    if num_args == 1:
+        print("{}".format(result))
+    else:
+        while i > 0 and i < num_args:
+            result = result + int(sys.argv[i])
+            i = i + 1
+        print("{}".format(result))
