d727408bd53abb2dcae7f18f59adbebe0df940c5
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-10 22:41:37 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,9 +6,9 @@ result = 0
 num_args = len(sys.argv)
 
 if num_args == 1:
-    print(f"{result}")
+    print("{}".format(result))
 else:
     while i > 0 and i < num_args:
         result = result + int(sys.argv[i])
         i = i + 1
-    print(f"{result}")
+    print("{}".format(result))
