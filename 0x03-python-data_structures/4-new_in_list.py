da7eb7235c107053d8522d93a537533dcc8620ad
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-12 12:35:34 +0100

    update

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """
+    a function that replaces an element in a list at a specific position without modifying the original list.
+    """
+
+    new_list = list.copy(my_list)
+
+    if idx < 0:
+        return new_list
+    elif idx > (len(new_list) - 1):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
