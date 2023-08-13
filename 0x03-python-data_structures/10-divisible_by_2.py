cab2e87b1f260cfe9a1fef907d1cc299e56fd26e
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-13 16:56:23 +0100

    update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """
+    a function that finds all the multiples of
+    2 in a list
+    """
+
+    new_list = []
+
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+
+        else:
+            new_list.append(False)
+    return new_list
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    finds the biggest integer of a list
+    """
+
+    if len(my_list) == 0:
+        return None
+    else:
+        max_num = my_list[0]
+
+        for num in my_list:
+            if num > max_num:
+                max_num = num
+        return max_num
