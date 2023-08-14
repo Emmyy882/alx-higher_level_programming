a8e059b2524db6ac33e82936a036f82bbf9bd840
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-14 22:43:55 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end=" " if num != row[-1] else "")
         print()
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,6 +7,6 @@ def multiple_returns(sentence):
     """
 
     if len(sentence) == 0:
-        return (None, None)
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
