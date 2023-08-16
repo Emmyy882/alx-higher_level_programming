c0eb4ca8022d9f86f17eef2be8acc805325de2b6
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-16 19:06:52 +0100

    update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -5,7 +5,7 @@ def number_keys(a_dictionary):
     a function that returns the number of keys
     in a dictionary
     """
-    
+
     num = 0
     for k in a_dictionary.items():
         num += 1
