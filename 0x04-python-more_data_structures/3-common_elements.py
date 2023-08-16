d9625df4e07683008fb032f41324fdf78bb6e8e4
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-16 18:02:20 +0100

    update

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    a function that returns a set of common ele
+    ments in two sets
+    """
+
+    result = set()
+    result = set_1 & set_2
+
+    return result
