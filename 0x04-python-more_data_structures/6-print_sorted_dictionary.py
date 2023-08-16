b45395aa0f8fa0eab526df439bed28f85d58efa8
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-16 19:54:47 +0100

    update

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    a function that prints a dictionary by 
+    ordered keys
+    """
+
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
