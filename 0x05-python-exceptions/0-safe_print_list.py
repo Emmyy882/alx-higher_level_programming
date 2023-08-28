1065f38fb7be54001d6f3c4e94281b7edef7e3d4
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-28 19:43:45 +0100

    printing the elements on a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """
+    a function that prints x elements of a list
+    """
+
+    try:
+        count = 0
+        for element in my_list:
+            if count < x:
+                print("{}".format(element), end="")
+                count += 1
+        print()
+        return count
+    
+    except:
+        return count
