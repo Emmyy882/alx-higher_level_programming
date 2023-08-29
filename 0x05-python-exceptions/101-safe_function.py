0810daa8f48cd6bf857a8a0e2454dc5204c91a8d
Emmyy882 <emmaagbaje3@gmail.com>
2023-08-29 01:26:15 +0100

    update

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    """
+    executes a function safely
+    """
+
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
