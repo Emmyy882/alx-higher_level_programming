f2bcf727a8577ab4f264402e393da0e01459685a
Emmyy882 <emmaagbaje3@gmail.com>
2023-09-12 22:16:48 +0100

    file input and output in python

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""a function that returns the JSON representation of an object..
+........(string).
+"""
+
+
+def to_json_string(my_obj):
+    """
+    a function that returns the JSON representation of an object..
+    ....(string).
+    """
+    json.dumps(my_obj)
