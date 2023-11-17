4f98553a5cb17a9fdf5eddd35524f719de0f6d05
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-17 10:34:51 +0100

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,10 @@ if __name__ == '__main__':
                 db=sys.argv[3]
                 )
         cursor = db_conn.cursor()
-        cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC;')
+        cursor.execute(
+            'SELECT * FROM states WHERE name IS NOT NULL AND' +
+            ' LEFT(CAST(name AS BINARY), 1) = "N" ORDER BY states.id ASC;'
+        )
         results = cursor.fetchall()
         for result in results:
             print(result)
