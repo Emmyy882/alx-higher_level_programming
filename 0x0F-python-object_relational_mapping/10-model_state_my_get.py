1562fd9e1e526ff0b3ec292b7f109c0f80fd5d5d
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-18 21:27:20 +0100

    update

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument from the \
+        database hbtn_0e_0_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+
+        # connecting to MySQL server
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                pool_pre_ping=True
+                )
+
+        # Create all tables in the database
+        Base.metadata.create_all(engine)
+
+        # creating a session to interact with the database
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        state_name = sys.argv[4]
+        # Printing the first object
+        result = session.query(State).filter(State.name==state_name).first()
+        if result:
+            print('{}'.format(result.id))
+        else:
+            print("Not found")
---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""adds the State object 'Louisiana' to the database hbtn_0e_0_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+
+        # connecting to MySQL server
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                pool_pre_ping=True
+                )
+
+        # Create all tables in the database
+        Base.metadata.create_all(engine)
+
+        # creating a session to interact with the database
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Adding the State object
+        new_state = State(name = 'Louisiana')
+        session.add(new_state)
+        session.commit()
+
+        # Printing the new states.id
+        print('{}'.format(new_state.id))
