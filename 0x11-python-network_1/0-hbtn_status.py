61d8a44d58f1ebf69e4e5a3240c0f134423c2de6
Emmyy882 <emmaagbaje3@gmail.com>
2023-12-01 21:43:34 +0100

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         body = response.read()
 
-        print("body response:")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
