1afdaa9e53fd57a985e38e589483bf940e31424f
Emmyy882 <emmaagbaje3@gmail.com>
2023-12-01 21:31:56 +0100

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 import urllib.request
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    
+
     with urllib.request.urlopen(url) as response:
         body = response.read()
 
